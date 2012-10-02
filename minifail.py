b36d47cda9f438911bb7153324a0eec65b884ee8
Gautier Hayoun <ghayoun@gmail.com>
2012-10-02 14:32:22 +0100

    WIP

--- minifail.py
+++ minifail.py
@@ -1,11 +1,12 @@
 """Usage:
-    minifail.py listen <interface> <ip>
-    minifail.py heartbeat <interface> <ip>
+    minifail.py <interface> <ip>
 """
 
 import socket
 import time
 import sys
+import struct
+import socket
 import subprocess
 
 from docopt import docopt
@@ -33,40 +34,107 @@ def listen(ip):
     failures = 0
     while True:
         sys.stdout.flush()
-        try:
-            data, addr = sock.recvfrom(16)
-        except socket.timeout as e:
-            print e.message
-            failures += 1
-        else:
-            failures = 0
-            print ("received", data, "from", addr)
 
         if failures == 3:
             print "DIED"
             return
 
+def loop(listen_ip, bcast_ip, has_ip, ip):
+    broadcast_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    broadcast_sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+    broadcast_sock.setblocking(0)
+
+    listen_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    listen_sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    listen_sock.setblocking(0)
+    listen_sock.bind((bcast_ip, 1694))
+
+    failures = 0
+
+    while True:
+        # check conflicts
+
+        if has_ip:
+            message = "X"
+            sent = broadcast_sock.sendto(message, (bcast_ip, 1694))
+            if sent != len(message):
+                error("heartbeat sending failed")
+        else:
+            try:
+                data, addr = listen_sock.recvfrom(16)
+            except socket.error as e:
+                print e.message
+                failures += 1
+            else:
+                failures = 0
+                print ("received", data, "from", addr)
+
+            if failures == 3:
+                # assign ip
+                yield "failover"
+                continue
+
+        yield None
+
+def unpack_str_ip(ip):
+    return struct.unpack("!L", socket.inet_pton(socket.AF_INET, ip))[0]
+
+def make_network(addr, netmask):
+    addr = unpack_str_ip(addr)
+    netmask = unpack_str_ip(netmask)
+    network = addr & netmask
+    return network
+
+def in_network(ip, network):
+    ip = unpack_str_ip(ip)
+
 def main():
     arguments = docopt(__doc__, argv=sys.argv[1:], help=True, version=None)
     interface_name = arguments["<interface>"]
-    interface = netifaces.ifaddresses(interface_name)
-    if socket.AF_INET not in interface:
-        error("Interface %s doesn't have IPV4 address (probably not up)"
-                % interface_name)
-    print interface
+    ip = arguments["<ip>"]
 
-    #for interface in netifaces.interfaces():
-    #    addresses = 
-    #    if socket.AF_INET in addresses:
-    #        print interface
-    #        print addresses[socket.AF_INET]
-    #        print
-    return
+    bcast_ip = None
+    has_ip = False
+    listen_ip = None
 
-    if arguments["listen"]:
-        listen(arguments["<IP>"])
-    elif arguments["heartbeat"]:
-        heartbeat(arguments["<IP>"])
+    interfaces = [interface for interface in netifaces.interfaces()
+                            if interface.startswith(interface_name)]
+
+    for interface in interfaces:
+        addresses = netifaces.ifaddresses(interface)
+        if socket.AF_INET not in addresses:
+            continue
+
+        for address in addresses[socket.AF_INET]:
+            # right network
+            broadcast = address['broadcast']
+            addr = address['addr']
+            netmask = address['netmask']
+            network = make_network(addr, netmask)
+
+            if make_network(ip, netmask) == network:
+                bcast_ip = broadcast
+                listen_ip = addr
+
+                if ip == addr:
+                    has_ip = True
+
+        #if socket.AF_INET not in interface:
+        #    error("Interface %s doesn't have IPV4 address (probably not up)"
+        #            % interface_name)
+
+    if has_ip:
+        print "IP is assigned to this machine, start bcast_ip", ip, bcast_ip
+        print "XXX Should check for conflict"
+    else:
+        print "IP is not assigned to this machine, start listening", ip, bcast_ip
+
+    for message in loop(listen_ip, bcast_ip, has_ip):
+        subprocess.call(["ifconfig", "eth0:0", "10.0.0.3", "netmask", "255.0.0.0", "up"])
+        has_ip = True
+        if message == "failure":
+            pass
+        time.sleep(.5)
 
 if __name__ == "__main__":
     main()
