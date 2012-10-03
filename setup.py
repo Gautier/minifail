d1e02478793e0f035d4bafacf3a8556ba1dd1df9
Gautier Hayoun <ghayoun@gmail.com>
2012-10-03 17:23:57 +0100

    Use setup.py for dependency

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='minifail',
+    version='0.1',
+    description='',
+    author='Gautier Hayoun',
+    license='Private',
+    packages=find_packages(),
+    install_requires=['docopt==0.5.0'],
+)
+
+
