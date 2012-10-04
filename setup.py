4b1db4d28f23755279d6456114a73af9e94ecb64
Gautier Hayoun <ghayoun@gmail.com>
2012-10-04 17:10:01 +0100

    Add missing required information from setup.py

--- setup.py
+++ setup.py
@@ -3,9 +3,11 @@ from setuptools import setup, find_packages
 setup(
     name='minifail',
     version='0.1',
-    description='',
+    description='minifail is a simple IP failover compatible with Linux/BSD/MacOS',
     author='Gautier Hayoun',
-    license='Private',
+    author_email='ghayoun@gmail.com',
+    url='https://github.com/gautier/minifail',
+    license='MIT',
     packages=find_packages(),
     entry_points={
         'console_scripts': [
